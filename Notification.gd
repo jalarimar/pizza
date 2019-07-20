472183d89108795caf1272fd0d999977ba5d0755
Maxwell Clarke <maxeonyx@gmail.com>
2019-07-20 17:44:31 +1200

    Change image of notification.

--- Notification.gd
+++ Notification.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 var speak_range = 0
 var circle_radius = 0
+
+const modifier = 1.2
+const scale_base = Vector2(0.2, 0.2)
+const radius_base = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Tween.interpolate_property($Sprite, "scale", Vector2(0.1, 0.1), Vector2(speak_range, speak_range), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-    $Tween.interpolate_property(self, "circle_radius", 0, speak_range * 100, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    $Tween.interpolate_property($Sprite, "scale", Vector2(0.05, 0.05), speak_range * scale_base * modifier, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    $Tween.interpolate_property(self, "circle_radius", 10, speak_range * radius_base * modifier, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
     $Tween.start()
     yield($Tween, "tween_completed")
     queue_free()
@@ -15,3 +20,9 @@ func _physics_process(delta):
     for body in get_overlapping_bodies():
         if body.has_method("hear_pizza"):
             body.hear_pizza()
+            
+func _process(delta):
+    update()
+            
+func _draw():
+    draw_circle(Vector2(), $CollisionShape2D.shape.radius, Color.black)
